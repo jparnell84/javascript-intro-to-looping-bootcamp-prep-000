0f51abf19af78471bbe59b98489f553501a79f64
Jonathan Parnell <jonathan.parnell1@gmail.com>
2020-08-24 22:58:22 +0000

    Automatically backed up by Learn

---
+++ loops.js
@@ -0,0 +1,15 @@
+function forLoop(){
+var strangeLoop = [];
+for (let i = 0; i < 25; i++) {
+  if (i === 0){
+  return ["I am 1 strange loop."];
+  } else {
+    return ["I am ${i} strange loops."];
+  }
+ }
+}
+//function whileLoop(number){
+  //while (n > 0){
+    //console.log('')
+  //}
+//}
